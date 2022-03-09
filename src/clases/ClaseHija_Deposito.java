07e7ba51bad9e56b06b716c8f8e3cf3d85dfe2fe
XavCilia <xavcilia@gmail.com>
2022-03-09 13:34:35 -0600

    <?xml version="1.0" encoding="UTF-8"?> <!-- You may freely edit this file. See commented blocks below for --> <!-- some examples of how to customize the build. --> <!-- (If you delete it and reopen the project it will be recreated.) --> <!-- By default, only the Clean and Build commands use this build script. --> <!-- Commands such as Run, Debug, and Test only use this build script if --> <!-- the Compile on Save feature is turned off for the project. --> <!-- You can turn off the Compile on Save (or Deploy on Save) setting --> <!-- in the project's Project Properties dialog box.--> <project name="CajeroAutomatico" default="default" basedir=".">     <description>Builds, tests, and runs the project CajeroAutomatico.</description>     <import file="nbproject/build-impl.xml"/>     <!--
        There exist several targets which are by default empty and which can be 
        used for execution of your tasks. These targets are usually executed 
        before and after some main targets. They are: 
    
          -pre-init:                 called before initialization of project properties
          -post-init:                called after initialization of project properties
          -pre-compile:              called before javac compilation
          -post-compile:             called after javac compilation
          -pre-compile-single:       called before javac compilation of single file
          -post-compile-single:      called after javac compilation of single file
          -pre-compile-test:         called before javac compilation of JUnit tests
          -post-compile-test:        called after javac compilation of JUnit tests
          -pre-compile-test-single:  called before javac compilation of single JUnit test
          -post-compile-test-single: called after javac compilation of single JUunit test
          -pre-jar:                  called before JAR building
          -post-jar:                 called after JAR building
          -post-clean:               called after cleaning build products
    
        (Targets beginning with '-' are not intended to be called on their own.)
    
        Example of inserting an obfuscator after compilation could look like this:
    
            <target name="-post-compile">
                <obfuscate>
                    <fileset dir="${build.classes.dir}"/>
                </obfuscate>
            </target>
    
        For list of available properties check the imported 
        nbproject/build-impl.xml file. 
    
    
        Another way to customize the build is by overriding existing main targets.
        The targets of interest are: 
    
          -init-macrodef-javac:     defines macro for javac compilation
          -init-macrodef-junit:     defines macro for junit execution
          -init-macrodef-debug:     defines macro for class debugging
          -init-macrodef-java:      defines macro for class execution
          -do-jar:                  JAR building
          run:                      execution of project 
          -javadoc-build:           Javadoc generation
          test-report:              JUnit report generation
    
        An example of overriding the target for project execution could look like this:
    
            <target name="run" depends="CajeroAutomatico-impl.jar">
                <exec dir="bin" executable="launcher.exe">
                    <arg file="${dist.jar}"/>
                </exec>
            </target>
    
        Notice that the overridden target depends on the jar target and not only on 
        the compile target as the regular run target does. Again, for a list of available 
        properties which you can use, check the target you are overriding in the
        nbproject/build-impl.xml file. 
    
        -->
    </project>

---
+++ src/clases/ClaseHija_Deposito.java
@@ -0,0 +1,19 @@
+
+package clases;
+
+public class ClaseHija_Deposito extends ClasePadre_Abstracta{
+    @Override
+    public void Transacciones(){
+        System.out.println("¿Cuanto deseas depositar?");
+        Depósito() ;
+        
+        transacciones = getSaldo();
+        setSaldo(transacciones + deposito);
+        System.out.println("-----------------------");
+        System.out.println("Depositaste: " + deposito);
+        System.out.println("Tu saldo actual es: " + getSaldo());
+        System.out.println("-----------------------");
+        
+    }
+    
+}
